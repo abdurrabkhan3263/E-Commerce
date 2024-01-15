9dcc1ff8717eee070509efb5ea387eb657d04a5a
Abdur Rab Khan <abdurrabkhan3263@gmail.com>
2024-01-15 22:51:42 +0530

    Third Commit

--- script.js
+++ script.js
@@ -13,3 +13,37 @@ menuBar.addEventListener('click' , (event)=>{
 navClose.addEventListener('click' , function(){
     openClose();
 })
+
+
+// OPENING THE PRODUCT;
+const product = document.querySelectorAll('.pro');
+
+function productOpen(){
+    window.location.href = 'sproduct.html'
+}
+
+function cartOpen(event){
+    event.target.parentNode.href = 'cart.html'
+}
+
+product.forEach((value)=>{
+    value.addEventListener('click' , (event)=>{
+        if(event.target.tagName === 'IMG' || event.target.tagName === 'H5'){
+            let name = value.querySelector('.des')
+            let img = value.firstElementChild.src;
+            let proName = name.firstElementChild.nextElementSibling.innerHTML;
+            let price = name.querySelector('h4').innerHTML;
+            productOpen();
+            let arr = [{
+                img : img,
+                proName : proName,
+                price : price,
+            }]
+            localStorage.setItem('data' , JSON.stringify(arr))
+        }
+        if(event.target.tagName === 'I'){
+            cartOpen(event);
+        }
+    })
+})
+
